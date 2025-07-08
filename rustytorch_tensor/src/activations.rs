72f8705ab7a5a0046c903f370d8bd934ef73a808
y_mc <momo.toure27@gmail.com>
2025-07-08 19:16:46 +0200

    update

--- rustytorch_tensor/src/activations.rs
+++ rustytorch_tensor/src/activations.rs
@@ -1,10 +1,8 @@
 // rustytorch_tensor/src/activations.rs
 
 use crate::Tensor;
-use crate::storage::StorageType;
-use crate::tensor_errors::{TensorError, TensorErrorType};
-use std::sync::Arc;
-use rustytorch_core::NumericOps;
+
+// use rustytorch_core::NumericOps;
 
 impl Tensor {
     // /// Applique la fonction d'activation ReLU (Rectified Linear Unit)
@@ -62,56 +60,6 @@ impl Tensor {
     //     let grad = one.sub(&tanh_squared)?;
     //     grad.mul(grad_output.clone())
     // }
-
-    /// Compare élément par élément si les éléments du tenseur sont supérieurs à ceux d'un autre tenseur
-    pub fn gt(&self, other: &Self) -> Result<Self, TensorError> {
-        let result_shape = self.broadcast_shapes(other)?;
-
-        // Si les formes ne sont pas identiques, broadcaster
-        let self_broadcast = self.broadcast_to(&result_shape)?;
-        let other_broadcast = other.broadcast_to(&result_shape)?;
-
-        // Créer un tenseur booléen résultat
-        let mut result = Self::zeros(result_shape.clone(), Some(self.options.clone()));
-
-        // Comparer élément par élément
-        match (self_broadcast.storage.as_ref(), other_broadcast.storage.as_ref()) {
-            (StorageType::F32(a), StorageType::F32(b)) => {
-                let mut result_data = vec![false; a.len()];
-                for i in 0..a.len() {
-                    result_data[i] = a[i] > b[i];
-                }
-                result.storage = Arc::new(StorageType::from_bool(&result_data));
-            },
-            (StorageType::F64(a), StorageType::F64(b)) => {
-                let mut result_data = vec![false; a.len()];
-                for i in 0..a.len() {
-                    result_data[i] = a[i] > b[i];
-                }
-                result.storage = Arc::new(StorageType::from_bool(&result_data));
-            },
-            (StorageType::I32(a), StorageType::I32(b)) => {
-                let mut result_data = vec![false; a.len()];
-                for i in 0..a.len() {
-                    result_data[i] = a[i] > b[i];
-                }
-                result.storage = Arc::new(StorageType::from_bool(&result_data));
-            },
-            (StorageType::I64(a), StorageType::I64(b)) => {
-                let mut result_data = vec![false; a.len()];
-                for i in 0..a.len() {
-                    result_data[i] = a[i] > b[i];
-                }
-                result.storage = Arc::new(StorageType::from_bool(&result_data));
-            },
-            _ => return Err(TensorError::new(
-                TensorErrorType::TypeError,
-                "Incompatible types for comparison"
-            )),
-        }
-
-        Ok(result)
-    }
 
     // /// Applique une opération unaire optimisée élément par élément sur le tenseur
     // pub fn apply_unary_op<F32Op, F64Op>(&self, f32_op: F32Op, f64_op: F64Op) -> Result<Self, TensorError>
